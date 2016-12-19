e5f13c6ac78ba0a0123d04eb077dab3777f286f4
thelordmalcolm <lil_mal0@yahoo.com>
2016-12-18 21:05:18 -0500

    updates

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-
+#include "time.h"
 
 int main(){
 		
@@ -17,6 +17,8 @@ int main(){
 				"apple"
 		};
 		
+		char username[10];
+
 		// index for random word
 		int randomIndex = rand() % 6;
 		
@@ -26,22 +28,46 @@ int main(){
 		
 		//length of a word in the array
 		int wordLength = strlen(guessWords[randomIndex]);
+		//                       0 1 2 3 4 5 6 7
+		//  set value to 1
+		//  if the letter
+		//  is guessed
 		int letterGuessed[8] = { 0,0,0,0,0,0,0,0 };
 
 		int quit = 0;
 
 		char guess[16];
+		char letterEntered;
+		
+		printf("guessWords:%s \nrandomIndex:%d \nwordLength:%d\n\n",
+						guessWords[randomIndex],
+						randomIndex,
+						wordLength);
 
+		//game's function 
 		while(amountCorrect < wordLength){
 			
 				printf("Which letter will you guess?:");
-				fgets(guess, 16, stdin);
+				fgets(guess, 16, stdin); //reads line and stores it in str
 
 				if( strncmp(guess, "quit", 4) == 0 ){
 						quit =1;
 						break;
 				}
 
+				letterEntered = guess[0];
+				printf("You chose:%c\n", letterEntered);
+
+				for(int i=0; i<wordLength; i++ ){
+						if(letterGuessed[i] == 1){
+								continue;
+						}
+						
+						if( letterEntered == guessWords[randomIndex][i] ){
+								letterGuessed[i] = 1;
+								amountCorrect++;
+						}
+				}
 		}
 
 		if(quit == 1){
